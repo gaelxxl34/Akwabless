a117f7faf63f4eac8c45b0b21ef88ddd74a16021
esdraswite <witeesdras@gmail.com>
2025-05-17 11:04:56 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/not-found/NotFound";
+import Home from "./pages/home/Home";
+import Login from "./features/auth/Login";
+import Registration from "./features/auth/Registration";
+import AdminLayout from "./pages/layouts/AdminLayout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      // element: <HomeLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/registration",
+          element: <Registration />,
+        },
+        {
+          path: "/admin",
+          element: <AdminLayout />,
+          children: [
+            {
+              path: "/admin",
+              element: <>Dashoard</>,
+            },
+            {
+              path: "/admin/users",
+              element: <>users</>,
+            },
+            {
+              path: "/admin/projects",
+              element: <>projects</>,
+            },
+            {
+              path: "/admin/donations",
+              element: <>donations</>,
+            },
+            {
+              path: "/admin/stories",
+              element: <>stories</>,
+            },
+            {
+              path: "/admin/brochure-generator",
+              element: <>brochure-generator</>,
+            },
+            {
+              path: "/admin/settings",
+              element: <>settings</>,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
